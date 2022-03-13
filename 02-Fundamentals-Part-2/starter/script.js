16a46a254ec799ca50d9f18c390fbd4a9bc99943
E1ZPWR <jacklisiyuan@gmail.com>
2022-03-13 22:55:01 +1100

    Updated

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -305,7 +305,13 @@ console.log(neighbours);
 // 1. Create an object called 'myCountry' for a country of your choice, containing
 // properties 'country', 'capital', 'language', 'population' and
 // 'neighbours' (an array like we used in previous assignments)
-
+const myCountry = {
+    country: "Singapore",
+    capital: "Singapore",
+    language: "English",
+    population: 8782,
+    neighbours: ["China", "USA", "Japan"],
+};
 // ! LECTURE: Dot vs. Bracket Notation
 
 // ? Assignment - LECTURE: Dot vs. Bracket Notation
