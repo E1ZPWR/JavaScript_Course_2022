1d20819840cfa433bc25dda9136cbbe589d3163a
e1zpwr <jacklisiyuan@gmail.com>
2022-04-09 21:28:41 +1000

    Cha3 forEach

--- Coding challenges/03-Developer Skills & Editor Setup/Coding Challenge_01.js
+++ Coding challenges/03-Developer Skills & Editor Setup/Coding Challenge_01.js
@@ -19,9 +19,9 @@ dataOne = [17, 21, 23];
 dataTwo = [12, 5, -5, 0, 4];
 
 const printForecast = (arr) => {
-  for (let i = 0; i < arr.length; i++) {
-    console.log(`... ${arr[i]}ºC in ${i + 1} days ...`);
-  }
-}
+    arr.forEach((el, i) => {
+        console.log(`... ${el}ºC in ${i + 1} days ...`);
+    });
+};
 printForecast(dataOne);
 printForecast(dataTwo);
