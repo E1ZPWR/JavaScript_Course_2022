d8dcc6c321ee21c4bd4d497b7c08142fd5844398
Siyuan Li <jacklisiyuan@gmail.com>
2022-04-18 14:17:16 +1000

    DOM

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,8 @@
-'use strict';
+"use strict";
+
+const msg = document.querySelector(".message");
+msg.textContent = "Correct Number";
+
+numberTag = document.querySelector(".number");
+
+currentSocre = document.querySelector(".score");
