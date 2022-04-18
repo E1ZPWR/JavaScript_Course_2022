ad23e8b0094fc31cd53aa0487af546c99cafaed3
Siyuan Li <jacklisiyuan@gmail.com>
2022-04-18 18:55:31 +1000

    05_Guessing Number Project Final Update

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -3,78 +3,94 @@
 // Too High -> -1
 // Too Low -> -1
 
-const msg = document.querySelector(".message");
+// meesage show on the DOM
+const message = document.querySelector(".message");
 
+// The ? or the Number at the middle of the DOM
 const numberTag = document.querySelector(".number");
 
 // Button DOM element
 const checkBtn = document.querySelector(".check");
 const resetBtn = document.querySelector(".again");
 
-// score system
-const highscore = document.querySelector(".highscore");
-
 // Create a random number for Secret Number
 let randomNumber = Math.floor(Math.random() * 20) + 1;
 
-const scoreDom = document.querySelector(".score");
+//Score System value declaration
 let score = 20;
+let highscore = 0;
+const scoreDom = document.querySelector(".score");
+const highscoreDom = document.querySelector(".highscore");
 
+// Function for the game logic
 function main() {
     const guess = Number(document.querySelector(".guess").value);
     // When there is no gueesing number
     if (!guess) {
-        msg.textContent = "Please enter a number";
-        msg.style.color = "red";
-
+        msg("Please enter a number");
         // When Number is Correct
     } else if (randomNumber === guess) {
         // DOM text output
-        msg.textContent = "🧧 Correct Number";
+        msg("🧧 Correct Number");
         document.querySelector("body").style.backgroundColor = "green";
         numberTag.textContent = randomNumber;
         numberTag.style.width = "30rem";
-        // When number is too high
-    } else if (guess > randomNumber) {
+        // check the highest score only when you win the game.
+        HighestScore();
+    } else if (guess !== randomNumber) {
         if (score > 1) {
             // DOM text output
-            msg.textContent = "😓 Your Guessing is too High";
+            msg(
+                guess > randomNumber
+                    ? "😓 Your Guessing is too High"
+                    : "😓 Your Guessing is too Low"
+            );
+            //calculate the score and output to the DOM
             score--;
             scoreDom.textContent = score;
         } else {
-            msg.textContent = "😓 You Lose the game !";
-            scoreDom.textContent = 0;
-        }
-        // When number is too low
-    } else if (guess < randomNumber) {
-        if (score > 1) {
-            // DOM text output
-            msg.textContent = "😓 Your Guessing is too Low";
-            score--;
-            scoreDom.textContent = score;
-        } else {
-            msg.textContent = "😓 You Lose the game !";
+            msg("😓 You Lose the game !");
             scoreDom.textContent = 0;
         }
     }
 }
 
+// Functions for compare the highest score and save it
+function HighestScore() {
+    // if (highscore < score) {
+    //     highscore = score;
+    //     highscoreDom.textContent = highscore;
+    // } else if (highscore > score) {
+    //     highscore = highscore;
+    //     highscoreDom.textContent = highscore;
+    // } else {
+    //     highscore = highscore;
+    //     highscoreDom.textContent = highscore;
+    // }
+    highscore < score
+        ? ((highscore = score), (highscoreDom.textContent = highscore))
+        : ((highscore = highscore), (highscoreDom.textContent = highscore));
+}
+
+//Function foe display message
+function msg(msgText) {
+    message.textContent = msgText;
+}
+
+//Function for Reset
 function Reset() {
     randomNumber = Math.floor(Math.random() * 20) + 1;
     score = 20;
     scoreDom.textContent = score;
     numberTag.style.width = "20rem";
-    msg.textContent = "Start guessing...";
+    msg("Start guessing...");
     document.querySelector("body").style.backgroundColor = "#222";
     numberTag.textContent = "?";
     document.querySelector(".guess").value = "";
 }
-// addEventListener
+
+// EventListener
 // addEventListener is a method
 // addEventListener(Type of event, function)
-
-// click this button to check the value of input field
 checkBtn.addEventListener("click", main);
-
-// Click this button to reset the game
 resetBtn.addEventListener("click", Reset);
