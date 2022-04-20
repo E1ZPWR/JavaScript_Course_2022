b4a967c59401f117d14e2e995a00ee80770561d7
Siyuan Li <jacklisiyuan@gmail.com>
2022-04-20 21:06:49 +1000

    07 Pig Game rolling ftn Update

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -18,11 +18,70 @@ const btnRoll = document.querySelector('.btn--roll');
 // image DOM Elements
 const diceEl = document.querySelector('.dice');
 
-// player active background DOM Elements
-const playerActive = document.querySelector('.player--active');
+// activePlayer : to check which player is the player who currently playing
+// score : the socre holder for each player
+// currentScore : the current score of each player (after rolling the dice)
 
-// initial score
-score0El.textContent = 0;
-score1El.textContent = 0;
-current0El.textContent = 0;
-current1El.textContent = 0;
+let scores, currentScore, activePlayer, playing;
+
+// initial score function
+const init = () => {
+    scores = [0, 0];
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+    diceEl.classList.add('hidden');
+    player0El.classList.remove('player--winner');
+    player1El.classList.remove('player--winner');
+    player0El.classList.add('.player--active');
+    player1El.classList.remove('.player--active');
+};
+
+// roll a dice function
+const roll = () => {
+    // 1. generate a random number between 1 to 6
+    const randomNumber = Math.floor(Math.random() * 6) + 1;
+    // 2. display the dice
+    diceEl.classList.remove('hidden');
+    // 2.1 display the dice number via image
+    diceEl.src = `dice-${randomNumber}.png`;
+    // 3. check if the random number is equal to 1
+    if (randomNumber !== 1) {
+        // 3.1 add the random number to the current score
+        currentScore += randomNumber;
+        // 3.2 display the current score on DOM
+        document.querySelector(
+            `#current--${activePlayer}`
+        ).textContent = currentScore;
+    } else {
+        //reset score before switch to another player
+        currentScore = 0;
+        document.querySelector(
+            `#current--${activePlayer}`
+        ).textContent = currentScore;
+        // 4. switch to another player
+        // 4.1  if the random number is equal to 1, switch player
+        // if we change player 0, switch player 1
+        // #current--${activePlayer} 0 or 1
+        // In there we are checking the player who is currently playing
+        // if activePlayer is player 0, then activePlayer === 0 ? 1 : 0 will be 1 and vice versa
+        activePlayer = activePlayer === 0 ? 1 : 0;
+        // 4.2 remove the active class from the player who is currently playing
+        player0El.classList.toggle('player--active');
+        player1El.classList.toggle('player--active');
+    }
+};
+
+// hold score function
+const hold = () => {
+    console.log('hold');
+};
+
+// Event Listeners
+btnNew.addEventListener('click', init);
+btnHold.addEventListener('click', hold);
+btnRoll.addEventListener('click', roll);
