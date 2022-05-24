59aa6959b4cf543142032bfbfe29329856cc28f8
Siyuan Li <jacklisiyuan@gmail.com>
2022-05-24 14:43:10 +1000

    Cha 8 added

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+// ! An High-Level Overview of JavaScript
+
+// ! The JavaScript Engine and Runtime
+
+// ! Execution Contexts and The Call Stack
+
+// ! Scope and The Scope Chain
+
+// ! Scoping in Practice
+
+// ! Variable Environment: Hoisting and The TDZ
+
+// ! Hoisting and TDZ in Practice
+
+// ! The this Keyword
+
+// ! The this Keyword in Practice
+
+// ! Regular Functions vs. Arrow Functions
+
+// ! Primitives vs. Objects (Primitive vs. Reference Types)
+
+// ! Primitives vs. Objects in Practice
--- testing.js
+++
@@ -1,9 +0,0 @@
-numberSet = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-const Dowork = (array) => {
-    array.forEach((e) => {
-        console.log(e + 1)
-    })
-}
-
-Dowork(numberSet)
