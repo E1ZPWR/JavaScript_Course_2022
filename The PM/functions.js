70b3c0c7f6d6ad97708ceb64428d2418b98e7d88
Siyuan Li <jacklisiyuan@gmail.com>
2022-06-20 21:10:43 +1000

    cha 8-3 updated

--- The PM/functions.js
+++ The PM/functions.js
@@ -1,26 +1,42 @@
-// link btn
-const ReadMore = document.querySelector("#ReadMore");
+// link btn at the end od bio paragraph
+const KReadMore = document.querySelector("#KReadMore");
+const PReadMore = document.querySelector("#PReadMore");
+// div paragraph for the long story
+const kdiv = document.querySelector("#kdiv");
+const pdiv = document.querySelector("#pdiv");
 
-// div paragraph
-const long = document.querySelector("#long");
-
-//function to show content
-function Read() {
+//function to show the long story
+function readMoreBtnK() {
   /**
    * 默认按键字为'Read More'
    * 如果长故事为不显示（默认情况），则显示长故事，并且改变按键字为'Read Less'
    */
-  if (long.style.display === "none") {
-    ReadMore.innerHTML = "Read Less";
-    long.style.display = "block";
+
+  if (kdiv.style.display === "none") {
+    KReadMore.innerHTML = "Read Less";
+    kdiv.style.display = "block";
     /**
      * 此时长故事已经显示，再次点击，则不显示，并且改变按键字为显示
      */
   } else {
-    ReadMore.innerHTML = "Read More";
-    long.style.display = "none";
+    KReadMore.innerHTML = "Read More";
+    kdiv.style.display = "none";
+  }
+}
+
+function readMoreBtnP() {
+  if (pdiv.style.display === "none") {
+    PReadMore.innerHTML = "Read Less";
+    pdiv.style.display = "block";
+    /**
+     * 此时长故事已经显示，再次点击，则不显示，并且改变按键字为显示
+     */
+  } else {
+    PReadMore.innerHTML = "Read More";
+    pdiv.style.display = "none";
   }
 }
 
 // Event listener
-ReadMore.addEventListener("click", Read);
+KReadMore.addEventListener("click", readMoreBtnK);
+PReadMore.addEventListener("click", readMoreBtnP);
