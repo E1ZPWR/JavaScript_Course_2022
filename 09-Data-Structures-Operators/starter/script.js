e82badef1c3d5da539419911221b39d2bffa5da9
Siyuan Li <jacklisiyuan@gmail.com>
2022-07-06 18:04:35 +1000

    解构对象操作

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -11,6 +11,7 @@ const restaurant = {
     categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
     starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
     mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+    dessertsMenu: ['Cannoli', 'Tiramisu', 'Torta'],
 
     openingHours: {
         mon: {
@@ -53,15 +54,25 @@ const restaurant = {
         DeliveryAddress = '197 Gray Ave, Brooklyn, NY 11201',
         OrderedEntryDish = 1,
         OrderedMainDish = 0,
-        CustomerName = 'Jack'
+        CustomerName = `HouseHolder of ${DeliveryAddress}`
     }) {
         console.log(`
-        restaurantName : ${this.name}
-        Ordered for ${CustomerName}
-        Delivery time: ${DeliveryTime}
+        =====================================
+        Restaurant Name: ${this.name}
+        =====================================
+        Customer Name: ${CustomerName}
+        
+        Delivery Time: ${DeliveryTime}
+        
         Delivery Address: ${DeliveryAddress}
-        Ordered entry dish: ${this.starterMenu[OrderedEntryDish]}
-        Ordered main dish: ${this.mainMenu[OrderedMainDish]}`);
+        
+        Ordered:
+            Entry Dish: ${this.starterMenu[OrderedEntryDish]}
+            Main Dish: ${this.mainMenu[OrderedMainDish]}
+        =====================================
+        End of the Order List
+        =====================================
+            `);
     }
 };
 //解构数组操作
@@ -69,7 +80,7 @@ const [starter, main] = restaurant.order(2, 0);
 console.log(starter, main);
 //解构对象操作
 restaurant.orderDelivery({
-    CustomerName: 'Daniel',
+    //CustomerName: 'Daniel',
     OrderedEntryDish: 2,
     OrderedMainDish: 2,
     DeliveryTime: '12:00',
