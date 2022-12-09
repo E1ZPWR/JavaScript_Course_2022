d6a8024d8661e53b633a1f5e740802d85fd00b82
Siyuan Li <jacklisiyuan@gmail.com>
2022-12-09 21:45:19 +1100

    GitKarken Push

---
+++ 09-Data-Structures-Operators/starter/DestructuringObjects.js
@@ -0,0 +1,45 @@
+const restaurant = {
+    name: 'Classico Italiano',
+    location: 'Via Angelo Tavanti 23, Firenze, Italy',
+    categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+    dessertsMenu: ['Cannoli', 'Tiramisu', 'Torta'],
+
+    openingHours: {
+        monday: {
+            open: 10,
+            close: 22
+        },
+        tuesday: {
+            open: 10,
+            close: 22
+        },
+        wednesday: {
+            open: 10,
+            close: 22
+        },
+        thursday: {
+            open: 12,
+            close: 22
+        },
+        friday: {
+            open: 11,
+            close: 23
+        },
+        saturday: {
+            open: 0, // Open 24 hours
+            close: 24
+        },
+        sunday: {
+            open: 0, // Open 24 hours
+            close: 24
+        }
+    },
+
+    OpenTime: function (day) {
+        return `Open Time for ${day}: ${this.openingHours[day].open} - ${this.openingHours[day].close}`;
+    }
+};
+const [open, close] = restaurant.OpenTime('sunday').split(' - ');
+console.log(open, close);
